c9b4895f1198d064f85e45610915c866406bcbe4
Darrin Eden <darrin.eden@gmail.com>
2011-04-20 14:35:11 -0700

    oh-my-zsh shared repository recipe option

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "darrin@heavywater.ca"
 license          "Apache 2.0"
 description      "Installs/Configures oh-my-zsh"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.3.0"
+version          "0.4.0"
 
 depends          "git"
 depends          "zsh"
