05f9b6f7c3d4ab48ee3f434011f22781ad77bd96
Sean Escriva <sean.escriva@gmail.com>
2013-10-25 15:41:44 -0700

    Add name and update maintainer info

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
-maintainer       "Heavy Water Software Inc."
-maintainer_email "darrin@heavywater.ca"
+name             "oh-my-zsh"
+maintainer       "Heavy Water Ops, LLC"
+maintainer_email "support@hw-ops.com"
 license          "Apache 2.0"
 description      "Installs/Configures oh-my-zsh"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
