6f0dd5ee2aa9356bb01e3c2365802d88d9d915f0
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-09-25 18:46:39 +0200

    Updated metadata to work with new Berkshelf

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,14 @@
-maintainer       "Heavy Water Software Inc."
-maintainer_email "darrin@heavywater.ca"
-license          "Apache 2.0"
-description      "Installs/Configures oh-my-zsh"
+name 'oh-my-zsh'
+maintainer 'Fabio Napoleoni'
+maintainer_email 'f.napoleoni@gmail.com'
+license 'Apache 2.0'
+description 'Installs/Configures oh-my-zsh'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.4.2"
+version '0.4.2'
 
-depends          "git"
-depends          "zsh"
-depends          "users"
+depends 'git'
+depends 'zsh'
+depends 'users'
 
 %w( ubuntu debian
     centos redhat fedora ).each do |os|
