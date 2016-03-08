09e6c5a5152e0db8e503e564b3e4117bc892f901
Aaron Baer <aaron@hw-ops.com>
2016-03-08 00:54:34 -0800

    oh-my-zsh 0.4.4

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             "oh-my-zsh"
 maintainer       "Heavy Water Ops, LLC"
-maintainer_email "support@hw-ops.com"
+maintainer_email "support@heavywater.io"
 license          "Apache 2.0"
 description      "Installs/Configures oh-my-zsh"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.4.3"
+version          "0.4.4"
 
 depends          "git"
 depends          "zsh"
